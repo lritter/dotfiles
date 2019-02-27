20c75f87ad59bbda93a9a424d564d227170dcd01
Lincoln Ritter <lincoln.ritter@gmail.com>
2019-02-27 14:24:03 -0500

    Improve ssh setup

--- bash/setup_ssh.sh
+++ bash/setup_ssh.sh
@@ -9,19 +9,24 @@ alias rx='ssh -t -t bastion rx'
 # eval "$(ssh-agent -s)" > /dev/null 2>&1
 # ssh-add -K ~/.ssh/animoto_id_rsa > /dev/null 2>&1
 
-ssh-add -l &>/dev/null # See if any identies have been added
-if [ "$?" != 0 ]; then
-  # Look to see if we have information about the agent that is running
-  # If we do, get that info into the shell
-  test -r ~/.ssh-agent && \
-    eval "$(<~/.ssh-agent)" >/dev/null
-
-  # If we still don't have any identities, start up ssh agent and
-  # Put the needed info in .ssh-agent and eval it.
-  ssh-add -l &>/dev/null
+setup_ssh() {
+  ssh-add -l &>/dev/null # See if any identies have been added
   if [ "$?" != 0 ]; then
-    (umask 066; ssh-agent > ~/.ssh-agent)
-    eval "$(<~/.ssh-agent)" >/dev/null
-    ssh-add -K ~/.ssh/animoto_id_rsa
+    # Look to see if we have information about the agent that is running
+    # If we do, get that info into the shell
+    test -r ~/.ssh-agent && \
+      eval "$(<~/.ssh-agent)" >/dev/null
+
+    # If we still don't have any identities, start up ssh agent and
+    # Put the needed info in .ssh-agent and eval it.
+    ssh-add -l &>/dev/null
+    if [ "$?" != 0 ]; then
+      (umask 066; ssh-agent > ~/.ssh-agent)
+      eval "$(<~/.ssh-agent)" >/dev/null
+      ssh-add -K ~/.ssh/animoto_id_rsa
+    fi
   fi
-fi
+}
+
+export setup_ssh
+setup_ssh
