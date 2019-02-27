612db8d8640bf94a13bd02135f41b53208d347aa
Lincoln Ritter <lincoln.ritter@gmail.com>
2019-02-27 14:24:28 -0500

    Generalize keychain secrete functionality

--- bin/setup_github_security_token.sh
+++ bin/setup_github_security_token.sh
@@ -5,30 +5,45 @@ github_keychain_service_name='Github User Token'
 keychain_args="-a ${USER} -s '${github_keychain_service_name}' -w"
 gh_token_cmd="security find-generic-password $keychain_args"
 
-function read_token_from_keychain {
-  security find-generic-password -a ${USER} -s "${keychain_service_name}" -w
+function read_secret_from_keychain {
+  service_name="${1}"
+  security find-generic-password -a ${USER} -s "${service_name}" -w 
 }
 
-function add_new_github_token_to_keychain {
-  echo Please enter your github token:
-  read github_token
-  security add-generic-password -a ${USER} -s "${keychain_service_name}" -w ${github_token} > /dev/null
+function prompt_user_for_secret {
+  service_name="${1}"
+  message="${2}"
+  echo "${message}"
+  read secret
+  security add-generic-password -a ${USER} -s "${service_name}" -w ${secret} > /dev/null
 }
 
-function add_github_token_if_needed {
-  read_token_from_keychain > /dev/null
+function add_secret_to_keychain_if_needed {
+  service_name="${1}"
+  message="${2}"
 
+  read_token_from_keychain "${service_name}" > /dev/null
   if [ $? -ne 0 ]
   then
      # Add a new token
-     add_new_github_token_to_keychain
-     add_github_token_if_needed # This is a sanity check
-  fi
+     prompt_user_for_secret "${service_name}" "${message}"
+     add_github_token_if_needed "${service_name}" "${message}" # This is a sanity check
+  fi 
+}
+
+function print_env_command {
+  service_name="${1}"
+  env_var="${2}"
+  kc_args="-a ${USER} -s '${service_name}' -w"
+  kc_cmd="security find-generic-password $kc_args"
+  echo -e "Add:\n"\"export $env_var=\$\($kc_cmd\)\""\nto your .bashrc"
+}
+
+function add_github_token_if_needed {
+  p="Please enter your github token:"
+  add_secret_to_keychain_if_needed "${github_keychain_service_name}" "${p}"
 }
 
 add_github_token_if_needed
 
-# echo $gh_token_cmd
-# alias gh_token='$(security find-generic-password -s '"${keychain_service_name}"' -w)'
-
-echo -e "Add:\n"\"export GITHUB_TOKEN=\$\($gh_token_cmd\)\""\nto your .bashrc"
+print_env_command "${github_keychain_service_name}" "GITHUB_TOKEN" 
