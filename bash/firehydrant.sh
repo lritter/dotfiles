dff9e19874ae7c7dda917b4f900a878648059fcb
Lincoln Ritter <lincoln@firehydrant.io>
2019-12-11 14:18:43 -0500

    put-on-staging function

--- bash/firehydrant.sh
+++ bash/firehydrant.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+# fhconsole finds an appropriate container and runs the rails console.
+# The command takes and optional argument which is the k8s/kubectl context. If none
+# is given then the current context is used. If given, the context is swtiched (and 
+# will remain switched after the function exits)
 fhconsole() {
+  if [ ! -z "$1" ]; then
+    ctx="$1"
+    kubectl config use-context "$ctx" || return 1
+  fi
+
   kubectl -n laddertruck exec -ti $(kubectl -n laddertruck get pod -l 'name=rails' -o json | jq -r .items[0].metadata.name) bin/rails console
 }
 
+put-on-staging() {
+  git checkout staging && git fetch origin && git reset --hard origin/staging && git merge - && git checkout -
+}
+
 export fhconsole
+export put-on-staging
 export PROJECT_LADDERTRUCK_DIR=/Users/lritter/src/laddertruck
