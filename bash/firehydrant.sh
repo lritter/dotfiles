6b4afa77849f9f9b945916f6b588fdbfc03ad16c
Lincoln Ritter <lincoln@firehydrant.io>
2019-12-31 13:31:02 -0500

    bash and sql fh commands

--- bash/firehydrant.sh
+++ bash/firehydrant.sh
@@ -13,6 +13,24 @@ fhconsole() {
   kubectl -n laddertruck exec -ti $(kubectl -n laddertruck get pod -l 'name=rails' -o json | jq -r .items[0].metadata.name) bin/rails console
 }
 
+fhbash() {
+  if [ ! -z "$1" ]; then
+    ctx="$1"
+    kubectl config use-context "$ctx" || return 1
+  fi
+
+  kubectl -n laddertruck exec -ti $(kubectl -n laddertruck get pod -l 'name=rails' -o json | jq -r .items[0].metadata.name) bash
+}
+
+fhsql() {
+  if [ ! -z "$1" ]; then
+    ctx="$1"
+    kubectl config use-context "$ctx" || return 1
+  fi
+
+  kubectl -n laddertruck exec -ti $(kubectl -n laddertruck get pod -l 'name=rails' -o json | jq -r .items[0].metadata.name) -- bash -c 'psql $DATABASE_URL' 
+}
+
 put-on-staging() {
   git checkout staging && git fetch origin && git reset --hard origin/staging && git merge - && git push && git checkout -
 }
